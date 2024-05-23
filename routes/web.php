0378ef8f1da63a07622a8345af8b8e2d8a9e1f10
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2024-05-23 12:41:42 +0200

    Install Laravel Broadcasting

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\EnvironmentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,14 +12,26 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [EnvironmentController::class, 'index'])
+    ->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [EnvironmentController::class, 'index'])->name('dashboard');
+
+    Route::get('/environment/new', function () {
+        return Inertia::render('EnvironmentForm');
+    })->name('environment.new');
+    Route::get('/environment/control/{vmid}/{option}', [EnvironmentController::class, 'controlEnvironment'])->name('environment.control');
+    Route::get('/environment/delete/{vmid}', [EnvironmentController::class, 'deleteEnvironment'])->name('environment.delete');
+    Route::get('/environment/details/{id}', function (int $id) {
+        $vm = Http::get('http://192.168.1.20/cnc/');
+    })->name('environment.details');
+});
+
 
 Route::post('/create', function (Request $request) {
     return dd($request);
