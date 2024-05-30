9131fde272b4df331f51603ac54d207ce448652a
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2024-05-30 21:51:55 +0200

    WIP on creating an HttpClient that will grab new JSON Web Tokens

--- app/Services/HttpService.php
+++ app/Services/HttpService.php
@@ -2,12 +2,30 @@
 
 namespace App\Services;
 
+use GuzzleHttp\Promise\PromiseInterface;
+use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 
-class HttpService
+class HttpService extends Http
 {
-    public function getToken()
+    public static string $token;
+
+    public static function get(string $url, array|string|null $query = null): PromiseInterface|Response
     {
-        // TODO snak med jacob om bedste implemenatation 
+        $response = parent::get($url, $query);
+        // If unauthorized, collect a token and retry the request
+        if ($response->status() === 401) {
+            self::$token = self::getToken();
+            $response = parent::withToken(self::$token)->get($url, $query);
+        }
+
+        return $response;
+    }
+
+    public static function getToken(): string
+    {
+        // TODO: Put in proper endpoint
+        return parent::get(config('app.api.endpoint'))->body();
     }
 }
