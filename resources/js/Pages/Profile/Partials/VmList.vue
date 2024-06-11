d17bd7bb95ca928aec86ac5ff0301024b36143e6
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2024-06-11 13:51:13 +0200

    Improved front-end dark mode visuals

--- resources/js/Pages/Profile/Partials/VmList.vue
+++ resources/js/Pages/Profile/Partials/VmList.vue
@@ -48,11 +48,6 @@
                         <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                             <MenuItems class="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                                 <MenuItem v-slot="{ active }">
-                                    <a href="#" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']"
-                                    >Rename<span class="sr-only">, {{ vm.name }}</span></a
-                                    >
-                                </MenuItem>
-                                <MenuItem v-slot="{ active }">
                                     <a :href="route('environment.delete', { environment: vm.id })" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']"
                                     >Delete<span class="sr-only">, {{ vm.name }}</span></a
                                     >
