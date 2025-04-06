6094915d3a558d40eb393666c9ef0423f461f8bd
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2025-04-06 18:26:48 +0200

    feat: add gate and HTTP macro

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +25,32 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::shouldBeStrict(! $this->app->isProduction());
+
+        Gate::define('interact-with-servers', function (User $user) {
+            return $user->role === 'admin';
+        });
+
+        Http::macro('proxmox', function () {
+            $token = Cache::get('token');
+            $tokenExpiration = Cache::get('token_expiration');
+
+            if ($token && $tokenExpiration && $tokenExpiration > now()) {
+                return Http::withToken(Cache::get('token'));
+            }
+
+            $response = Http::withHeaders([
+                'accept' => 'application/json',
+            ])->post(config('services.proxmox.endpoint'));
+
+            if ($response->successful()) {
+                $token = $response->json()['token'];
+                $tokenExpiration = $response->json()['expires_at'];
+                // TODO: Figure out TTL based on the tokenExpiration
+
+                // TODO: Cache::put('token', $token, $ttl);
+            }
+
+            return Http::withToken($token);
+        });
     }
 }
