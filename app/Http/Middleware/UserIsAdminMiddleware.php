575e2149b5b53ab875ad4e7bf42ddb5bbbfee4ff
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2025-04-09 08:54:16 +0200

    feat: prevent usage from non-admins

---
+++ app/Http/Middleware/UserIsAdminMiddleware.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class UserIsAdminMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->user()?->role !== 'admin') {
+            return redirect()->to('dashboard');
+        }
+        return $next($request);
+    }
+}
