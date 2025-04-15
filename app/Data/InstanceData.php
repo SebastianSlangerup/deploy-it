40619f2985143a940118f225f58a05e49444111d
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2025-04-15 14:11:14 +0200

    style: clean up dtos

--- app/Data/InstanceData.php
+++ app/Data/InstanceData.php
@@ -14,14 +14,12 @@ class InstanceData extends Data
         public string $name,
         public string $description,
         public UserData $created_by,
-
         public bool $is_ready,
         #[WithCast(InstanceStatusStateCast::class)]
         public InstanceStatusData $status,
         public ?Carbon $started_at,
         public ?Carbon $stopped_at,
         public ?Carbon $suspended_at,
-
         public Carbon $created_at,
         public Carbon $updated_at,
     ) {}
