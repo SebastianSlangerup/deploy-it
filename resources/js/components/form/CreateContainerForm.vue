07440c727e1ec7e4bf18d69c50be3a217966d3b0
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2025-04-17 15:31:49 +0200

    feat: working on container creation form

--- resources/js/components/form/CreateContainerForm.vue
+++ resources/js/components/form/CreateContainerForm.vue
@@ -1,7 +1,60 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import { useForm } from '@inertiajs/vue3';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import FormItem from '@/components/form/FormItem.vue';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+
+const form = useForm<{
+    name: string,
+    description: string,
+    docker_image: string,
+}>({
+    name: '',
+    description: '',
+    docker_image: '',
+})
+
+const submit = () => form.post(route('instances.store'));
+</script>
 
 <template>
-    <h1>Hej</h1>
-</template>
+    <form @submit.prevent="submit">
+        <Card class="m-0 lg:m-8">
+            <CardHeader>
+                <CardTitle>Create container instance</CardTitle>
+                <CardDescription>Create a new container instance</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Separator class="mb-6" label="Basic information" />
 
-<style scoped></style>
+                <FormItem name="name" label="Name" :form class="mb-2" is-required>
+                    <template #input>
+                        <Input type="text" placeholder="Name" v-model="form.name" />
+                    </template>
+                </FormItem>
+
+                <FormItem name="description" label="Description" :form class="mb-2" is-required>
+                    <template #input>
+                        <Textarea placeholder="Description" v-model="form.description" />
+                    </template>
+                </FormItem>
+
+                <Separator class="my-6" label="Docker Image" />
+
+                <FormItem name="docker-image" label="Docker Image" :form class="mb-2" is-required>
+                    <template #input>
+                        <Input type="text" placeholder="redis:alpine" v-model="form.docker_image" />
+                    </template>
+                </FormItem>
+            </CardContent>
+            <CardFooter class="flex justify-between px-6 pb-6">
+                <Button type="button" variant="outline">Cancel</Button>
+                <Button type="submit" :disabled="form.processing">Create</Button>
+            </CardFooter>
+        </Card>
+    </form>
+</template>
