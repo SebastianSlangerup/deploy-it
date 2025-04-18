5afce5c9200d49025062b674b0270aa8b147cc58
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2025-04-18 16:38:00 +0200

    feat: add docker image job

---
+++ app/Jobs/CreateDockerImageJob.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Instance;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+
+class CreateDockerImageJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(
+        public Instance $instance,
+        public string $dockerImage,
+    ) {}
+
+    public function handle(): void
+    {
+        try {
+            $response = Http::proxmox()
+                ->timeout(30)
+                ->withQueryParameters([
+                    'vmid' => $this->instance->vm_id,
+                    'image_name' => $this->dockerImage,
+                ])
+                ->post('/pull_docker_image');
+        } catch (ConnectionException $exception) {
+            Log::error('{job}: Connection failed. Retrying. Error message: {message}', [
+                'job' => "[ID: {$this->job->getJobId()}]",
+                'message' => $exception->getMessage(),
+            ]);
+
+            $this->release();
+
+            return;
+        }
+
+        if (! $response->successful()) {
+            Log::warning('{job}: Response unsuccessful. Message: {message}', [
+                'job' => "[ID: {$this->job->getJobId()}]",
+                'message' => $response->body(),
+            ]);
+
+            $this->release();
+        }
+
+        $this->instance->is_ready = true;
+        $this->instance->save();
+    }
+
+    public function failed(?Throwable $exception): void
+    {
+        $this->instance->delete();
+
+        Log::error('Job failed. Instance has been deleted. Message: {message}', [
+            'message' => $exception?->getMessage(),
+        ]);
+    }
+}
