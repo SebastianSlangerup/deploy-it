a983673b0b996889b4964a5a87fed304504e8fb1
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2025-04-24 15:33:27 +0200

    feat: add actions queue to horizon worker

--- config/horizon.php
+++ config/horizon.php
@@ -182,7 +182,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default', 'polling'],
+            'queue' => ['default', 'polling', 'actions'],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 1,
