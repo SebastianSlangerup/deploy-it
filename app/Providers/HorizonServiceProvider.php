34df61ae615d2ba2b68c048f583cbe4b288e6557
Sebastian Slangerup <sebastian.slangerup1@gmail.com>
2025-04-25 09:04:24 +0200

    fix: correct horizon gate

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Enums\RolesEnum;
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
 use Laravel\Horizon\HorizonApplicationServiceProvider;
@@ -27,8 +29,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewHorizon', function ($user) {
-            return $user->role === 'admin';
+        Gate::define('viewHorizon', function (User $user) {
+            return $user->role === RolesEnum::Admin;
         });
     }
 }
